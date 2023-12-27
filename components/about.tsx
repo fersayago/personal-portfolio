bd03e3ecf36923da451eaa13dd2a2560e84193a6
fersayago <sayago.fernando@gmail.com>
2023-12-27 15:51:10 -0300

    feat: :sparkles: New dependencies, header animations and section context

--- components/about.tsx
+++ components/about.tsx
@@ -4,12 +4,16 @@ import SectionHeading from './section-heading'
 import { motion } from 'framer-motion'
 
 const About = () => {
+  const ref = React.useRef(null)
+  
   return (
     <motion.section
-      className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40'
+      className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28'
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
+      id='about'
+      ref={ref}
     >
       <SectionHeading>
         About me
